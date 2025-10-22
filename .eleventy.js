6300a535cbfe38bd14f60e466c13856b39a81158
PinkuInraft <pinkuagarwal2831@gmail.com>
2025-10-22 16:21:19 +0530

    Fix: Configure Eleventy pathPrefix for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -19,6 +19,7 @@ module.exports = function(eleventyConfig) {
   });
 
   return {
+    pathPrefix: "/the-noise/", // For GitHub Pages deployment
     dir: {
       input: "src",
       output: "_site"
